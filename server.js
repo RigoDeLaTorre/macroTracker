6fc24a72664a906dc38a03aeb04642672d8d1b25
Rigo De La Torre <rigoddd@gmail.com>
2018-10-04 19:44:19 -0700

    h

--- server.js
+++ server.js
@@ -1,19 +1,14 @@
 var express = require('express');
 const path = require('path');
-var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
+
 var app = express();
-
-
-// make express look in the public directory for assets (css/js/img)
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use('/inde')
+app.use(express.static(path.resolve(__dirname, '/public')));
 app.get('/scrape', function(req, res) {
-  let username=req.query.username
-
-  getDailyStats =() =>{
-    url = `https://www.myfitnesspal.com/food/diary/${username}/`;
+    let username=req.query.username
+    url = `https://www.myfitnesspal.com/food/diary/${username}`
 
     request(url, function(error, response, html){
         if(!error){
@@ -36,28 +31,14 @@ app.get('/scrape', function(req, res) {
             json.fiber = parseFloat(fiber);
         })
     }
-     // To write to the system we will use the built in 'fs' library.
-     // In this example we will pass 3 parameters to the writeFile function
-     // Parameter 1 :  output.json - this is what the created filename will be called
-     // Parameter 2 :  JSON.stringify(json, null, 4) - the data to write, here we do an extra step by calling JSON.stringify to make our JSON easier to read
-     // Parameter 3 :  callback function - a callback function to let us know the status of our function
-
-    // fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
-    //     console.log('File successfully written! - Check your project directory for the output.json file');
-    // })
-
-     // Finally, we'll just send out a message to the browser reminding you that this app does not have a UI.
     res.send(json);
         }) ;
-  }
-  getDailyStats();
-
-})
+  })
 
 app.get('/weightInfo', function(req, res) {
-    let username=req.query.username
-    getWeightInfo = () => {
+    let username=req.query.username;
     url = `https://www.myfitnesspal.com/profile/${username}`;
+
     request(url, function(error, response, html) {
       if (!error) {
         var $ = cheerio.load(html);
@@ -85,13 +66,11 @@ app.get('/weightInfo', function(req, res) {
       }
       res.send(json);
     });
-  }
-    getWeightInfo();
+
   })
 
-  app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname,'/public/index.html'));
-});
+res.sendFile(path.resolve(__dirname+'/public', 'index.html'));
+
 app.listen(5000, () => {
   console.log('Server is running on port 5000')
 })
